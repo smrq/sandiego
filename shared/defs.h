cc2e31460aaba98ccd0631f86403da39fff22847
Greg Smith <smr.kyu@gmail.com>
2017-12-23 16:53:22 -0500

    Move deps into shared location

---
+++ shared/defs.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <avr/interrupt.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#define local static
+#define enableGlobalInterrupts sei
+#define disableGlobalInterrupts cli
+
+#define u8 uint8_t
+#define u16 uint16_t
+#define u32 uint32_t
+
+#define PIN_SS 2
+#define PIN_MOSI 3
+#define PIN_SCK 5
+
+#define TWI_BASE_ADDRESS 0x58
+#define TWI_CMD_SET_LED 0x10
+#define TWI_CMD_SET_ALL_LEDS 0x11
+#define TWI_CMD_SET_LED_PATTERN 0x12
+
+#define LED_COUNT 10
